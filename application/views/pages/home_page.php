19ef6ebf49315b2c52b622083511d3c4d8e4a1c4
Maneesha Rajaratne <maneesharajaratne@gmail.com>
2017-10-10 10:20:32 +0530

    maneesha's commit
    commented out all the newly added parts.

---
+++ application/views/pages/home_page.php
@@ -0,0 +1,20 @@
+    <div id="main">
+        <section id="showcase">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 showcase-content">
+                        <h2>Welcome to <span class="primary-text">UCSC Canteen</span></h2>
+                        <p><a class="btn btn-lg btn-warning" href="<?php echo base_url()?>/register" role="button">Sign up today</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <section id="content">
+        <div class="container">
+            <div class="row text-center">
+                <h2>Our Services</h2>
+            </div>
+
+        </div>
+    </section>
---
+++ application/views/pages/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maneesha
+ * Date: 10/8/2017
+ * Time: 7:16 PM
+ */
